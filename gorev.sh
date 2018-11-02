29058175f64f42805df283ce94f3b1779678572f
meliketrn <meliketuran8@hotmail.com>
2018-11-03 01:04:55 +0300

    mesaj

---
+++ gorev.sh
@@ -0,0 +1,27 @@
+! #/bin/bash
+ num=1 
+ x=$( ls -d */| sed 's#/##')
+
+ for i in $x
+ do  
+ echo  $num. "$i   $( ls $i | wc -l )"
+ ((num++))
+
+ done
+
+ 
+
+#! /bin/bash
+
+ 
+# # y=$( ls | wc -l )
+# # z=$( ls | wc -l )
+# # for i in $x $y
+
+# # do
+# # echo $num $i "=" $x
+# # echo $num $i "=" $z
+# # ((num++))
+# # done
+
+
