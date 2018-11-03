9a1b6339f52ea36e764cb22faa74c591072e1d47
meliketrn <meliketuran8@hotmail.com>
2018-11-03 15:42:35 +0300

    "Sıralanmış Dosyaların Toplamı" Eklendi

--- gorev.sh
+++ gorev.sh
@@ -5,10 +5,19 @@
  for i in $x
  do  
  echo  $num. "$i   $( ls $i | wc -l )"
- ((num++))
+  ((num++))
 
-
+ val1=$( ls | wc -l )
+ val2=($val1 + $( ls | wc -l ))
 
  done
+echo "toplam = $val2"
+ 
+
+ 
+
+
+
+
 
  
